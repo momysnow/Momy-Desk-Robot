82933107b28a185e804c8b30d56d11ebd0a4fdb9
momysnow <131729952+momysnow@users.noreply.github.com>
2023-11-07 19:25:47 +0100

    Add files via upload

--- boot/boot.ino
+++ boot/boot.ino
@@ -2,10 +2,24 @@
 #include <DHT.h>  // Include the DHT library
 #include "sd_read_write.h"
 #include "SD_MMC.h"
-#include <WiFi.h>
-#include "WifiManager.h"
 
-WifiManager wifiManager;
+#include <Arduino.h>
+#include <WiFi.h>
+#include <EEPROM.h>
+#include <WebServer.h>
+
+TaskHandle_t Loop;
+TaskHandle_t Client;
+
+WebServer server(80);
+
+// Dichiarazione delle reti Wi-Fi conosciute
+const char *knownNetworks[][2] = {
+  { "SSID1", "Password1" },
+  { "SSID2", "Password2" }
+  // Aggiungi altre reti conosciute qui
+};
+int numKnownNetworks = sizeof(knownNetworks) / sizeof(knownNetworks[0]);
 
 #define SD_MMC_CMD 38 //Please do not modify it.
 #define SD_MMC_CLK 39 //Please do not modify it. 
@@ -169,6 +183,17 @@ void sleep_eyes() {
 void setup() {
   dht.begin();
   Serial.begin(115200);
+  
+  //create a task that will be executed in the Task1code() function, with priority 1 and executed on core 0
+  xTaskCreatePinnedToCore(
+    handleClientTask, /* Task function. */
+    "Client", /* name of task. */
+    10000,  /* Stack size of task */
+    NULL, /* parameter of the task */
+    1,  /* priority of the task */
+    &Client,  /* Task handle to keep track of created task */
+    0 /* pin task to core 0 */ 
+  );
 
   SD_MMC.setPins(SD_MMC_CLK, SD_MMC_CMD, SD_MMC_D0);
   if (!SD_MMC.begin("/sdcard", true, true, SDMMC_FREQ_DEFAULT, 5)) {
@@ -176,40 +201,47 @@ void setup() {
     return;
   }
   uint8_t cardType = SD_MMC.cardType();
-  if(cardType == CARD_NONE){
+  if (cardType == CARD_NONE) {
     Serial.println("No SD_MMC card attached");
     return;
   }
-  
+
   tft.begin();
   tft.setRotation(0);
   tft.fillScreen(TFT_BLACK);
   tft.setSwapBytes(true);
 
   tft.pushImage(30, 60, logoWidth, logoHeight, logo_momysnow);
-  delay(2000);
+
+  const char* ssid = "YourSSID";  // Replace with your WiFi SSID
+  const char* password = "YourPassword";  // Replace with your WiFi password
+
+  if (!connect_wifi(ssid, password)) {
+    Serial.println("Failed to connect to Wi-Fi, starting access point");
+    start_ap();
+    create_web_server();
+    //tft.drawString("Cubot", 40, 40, 5);
+    //tft.drawString("Psw: Cubot1234", 30, 60, 10);
+  } else {
+    Serial.println("Connected to Wi-Fi");
+    create_web_server();
+    //tft.drawString("Connected to Wi-Fi", 40, 40, 4);
+  }
 
   tft.fillScreen(TFT_BLACK);
   delay(500);
-
-  if (!wifiManager.connect_wifi("ssid", "password")) {
-    Serial.println("Failed to connect to Wi-Fi, starting access point");
-    wifiManager.start_ap();
-    wifiManager.create_web_server();
-    tft.drawString("Cubot",40,40,5);
-    tft.drawString("Psw: Cubot1234",30,60,10);
-  } else {
-    Serial.println("Connected to Wi-Fi");
-    wifiManager.create_web_server();
-    tft.drawString("GAME OVER",40,40,4);
-  }
-
   eyes.createSprite(240, 240);
 }
 
-void loop() {
-  wifiManager.handleClient();
+void handleClientTask(void *param) {
+  while (true) {
+    // Handle client requests here
+    server.handleClient();
+    vTaskDelay(10); // Delay to yield to other tasks
+  }
+}
 
+void loop() {
   eyes.fillEllipse(x_eyeL, y_eyeL, w_eyes, h_eyes, TFT_WHITE);
   eyes.fillEllipse(x_eyeR, y_eyeR, w_eyes, h_eyes, TFT_WHITE);
   eyes.pushSprite(0, 0);
@@ -221,4 +253,83 @@ void loop() {
   dht.readTemperature();
   dht.readHumidity();
   delay(50);
+}
+
+bool connect_wifi(const char* ssid, const char* password) {
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  delay(100);
+
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  int retries = 0;
+  while (WiFi.status() != WL_CONNECTED && retries < 15) {
+    delay(500);
+    Serial.print(".");
+    retries++;
+  }
+  if (WiFi.status() == WL_CONNECTED) {
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+    return true;
+  } else {
+    Serial.println("");
+    Serial.println("WiFi connection failed");
+    return false;
+  }
+}
+
+void scan_wifi() {
+  int n = WiFi.scanNetworks();
+  Serial.println("Scan done");
+  if (n == 0) {
+    Serial.println("No networks found");
+  } else {
+    Serial.print(n);
+    Serial.println(" networks found");
+    for (int i = 0; i < n; ++i) {
+      Serial.print(WiFi.SSID(i));
+      Serial.print(" (");
+      Serial.print(WiFi.RSSI(i));
+      Serial.print(")");
+      Serial.println("");
+      delay(10);
+    }
+  }
+}
+
+void start_ap() {
+  WiFi.mode(WIFI_AP);
+  WiFi.softAP("Cubot", "Cubot1234");
+  IPAddress IP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(IP);
+}
+
+void create_web_server() {
+  server.on("/", HTTP_GET, handle_root);
+  // Define other routes here
+
+  server.begin();
+  Serial.println("Web server started");
+}
+
+
+
+void onRoot(void (*handler)()) {
+  server.on("/", handler);
+}
+
+void onNotFound(void (*handler)()) {
+  server.onNotFound(handler);
+}
+
+void handle_root() {
+  String html = "<html><body><h1>ESP32 Web Server</h1>";
+  server.send(200, "text/html", html);
 }
