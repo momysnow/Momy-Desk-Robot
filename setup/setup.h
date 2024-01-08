1d2533fc654716bbe6b1d7c78103e66acdbff054
momysnow <131729952+momysnow@users.noreply.github.com>
2024-01-08 02:22:32 +0100

    Add files via upload

---
+++ setup/setup.h
@@ -0,0 +1,16 @@
+// setup.h
+#ifndef SETUP_H
+#define SETUP_H
+
+// touchPad
+#define touchPadHeadPin 2
+
+// Servo Motors
+#define headServoPin 47
+#define pushLServoPin 48
+#define pushRServoPin 45
+#define rotateLServoPin 11
+#define rotateRServoPin 35
+#define baseServoPin 38
+
+#endif
