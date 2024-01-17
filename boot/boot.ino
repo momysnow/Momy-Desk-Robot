74f50c8cfe18fe42a9753639bfa9e77484fd9217
momysnow <131729952+momysnow@users.noreply.github.com>
2024-01-17 13:49:03 +0100

    Add files via upload

--- boot/boot.ino
+++ boot/boot.ino
@@ -5,6 +5,13 @@
 #include "SD_MMC.h"
 #include <ESP32Servo.h>
 
+#include "esp_camera.h"
+const char *server_ip = "192.168.1.123";  // Sostituisci con l'indirizzo IP del tuo server Python
+const int server_port = 8000;             // Sostituisci con la porta del tuo server Python
+
+int frameRate = 30;
+
+
 #include "animation.h"
 
 #include "microphone.h"
@@ -17,15 +24,13 @@ Preferences preferences;
 #include <WiFiManager.h>
 #include <ArduinoOTA.h>
 
-//ADDED*******************************************
-#include <PubSubClient.h>
-#include <SimpleTimer.h>
-//************************************************
-
 // task
+SemaphoreHandle_t taskSyncPinSemaphore;
+
 TaskHandle_t Animation;
 TaskHandle_t DHT11sensor;
 TaskHandle_t TouchPad;
+TaskHandle_t Streaming;
 
 // wifi
 const char* ota_host_name = "Momy-";
@@ -49,20 +54,6 @@ Servo rotateLServo;
 Servo rotateRServo;
 Servo baseServo;
 
-//Added**********************************************************
-//mqtt
-// Add your MQTT Broker IP address, example:
-const char* mqtt_server = "192.168.1.121";
-WiFiClient espClient;
-PubSubClient client(espClient);
-long lastMsg = 0;
-char msg[50];
-int value = 0;
-
-// Create a mqtt checkin simple timer
-SimpleTimer mqttCheckinTimer;
-//****************************************************************
-
 int pos = 0;
 int lastPosition = -1;  // Initialized to an impossible value
 
@@ -82,6 +73,8 @@ void setup() {
   // Load the large font
   tft.loadFont(AA_FONT_LARGE);
 
+  //camera
+  cameraSetup();
   //touch
   preferences.begin("touch", false);  // Apre le preferenze con il nome "touch", false indica che non è solo di lettura
   // setup dht
@@ -223,12 +216,6 @@ void setup() {
     tft.setCursor(centerX2, centerY2);
     tft.println(C);
 
-    //ADDED**************************************************************
-    //mqtt
-    client.setServer(mqtt_server, 1883);
-    client.setCallback(mqttcallback);
-    //*******************************************************************
-    
     // Procedi con altre operazioni di setup/inizializzazione
   }
 
@@ -240,22 +227,31 @@ void setup() {
   delay(500);
   eyes.createSprite(240, 240);
 
+
+  // Creare il semaforo
+  taskSyncPinSemaphore = xSemaphoreCreateBinary();
+
+  // Inizializzare il semaforo con un valore iniziale di "libero"
+  xSemaphoreGive(taskSyncPinSemaphore);
+
   // create TASK
   // create a task Animation
-  xTaskCreatePinnedToCore(AnimationTask, "Animation", 10000, NULL, 9, &Animation, 0);
+  xTaskCreatePinnedToCore(AnimationTask, "Animation", 15000, NULL, 9, &Animation, 1);
   // create a task DHT11
   xTaskCreatePinnedToCore(DHT11Task, "DHT11", 10000, NULL, 1, &DHT11sensor, 0);
   // create a task TouchPad
   xTaskCreatePinnedToCore(TouchTask, "TouchPad", 10000, NULL, 1, &TouchPad, 0);
+  // create a task Streaming
+  xTaskCreatePinnedToCore(StreamingTask, "Streaming", 30000, NULL, 8, &Streaming, 1);
 }
 
 void AnimationTask(void* param) {
   while (true) {
     idle_eyes();
-    delay(1000);
+    vTaskDelay(1000);
     wink_eyes();
 
-    delay(200);
+    vTaskDelay(200);
   }
 }
 
@@ -269,12 +265,19 @@ void TouchTask(void* param) {
   int touchPadThreshold = NTV + ((TV - NTV) /2);
 
   while (true) {
-    if (touchRead(touchPadHeadPin) > touchPadThreshold) {
-      //touched
-      //touch animation
-      //happiness++
+    if (xSemaphoreTake(taskSyncPinSemaphore, portMAX_DELAY)) {
+      // Controllo touch
+      if (touchRead(touchPadHeadPin) > touchPadThreshold) {
+        // Toccato
+        // Animazione touch
+        // Incremento di "happiness"
+      }
+
+      // Rilascio il semaforo
+      xSemaphoreGive(taskSyncPinSemaphore);
     }
-    delay(100);
+
+    vTaskDelay(100);
   }
 }
 
@@ -283,39 +286,73 @@ void DHT11Task(void* param) {
   const int umiditaCritica = 80;      // critical threshold for humidity
 
   while (true) {
-    float temperatura = dht.readTemperature();
-    float umidita = dht.readHumidity();
+    if (xSemaphoreTake(taskSyncPinSemaphore, portMAX_DELAY)) {
+      float temperatura = dht.readTemperature();
+      float umidita = dht.readHumidity();
 
-    // Check if the temperature or humidity exceeds critical thresholds
-    if (temperatura > temperaturaCritica || umidita > umiditaCritica) {
-      // print image high temperature
-      tft.fillScreen(TFT_BLACK);
-      tft.pushImage(30, 60, high_temperatureWidth, high_temperatureHeight, high_temperature);
+      // Check if the temperature or humidity exceeds critical thresholds
+      if (temperatura > temperaturaCritica || umidita > umiditaCritica) {
+        // print image high temperature
+        tft.fillScreen(TFT_BLACK);
+        tft.pushImage(30, 60, high_temperatureWidth, high_temperatureHeight, high_temperature);
 
-      delay(2000);
+        vTaskDelay(2000);
 
-      // it goes into rest mode
-      esp_sleep_enable_timer_wakeup(300000000);                         // Set the timer for 5 minutes
-      esp_sleep_pd_config(ESP_PD_DOMAIN_RTC_PERIPH, ESP_PD_OPTION_ON);  // Reactivate RTC
-      esp_light_sleep_start();
-      tft.fillScreen(TFT_BLACK);
+        // it goes into rest mode
+        esp_sleep_enable_timer_wakeup(300000000);                         // Set the timer for 5 minutes
+        esp_sleep_pd_config(ESP_PD_DOMAIN_RTC_PERIPH, ESP_PD_OPTION_ON);  // Reactivate RTC
+        esp_light_sleep_start();
+        tft.fillScreen(TFT_BLACK);
+      }
+
+      // Rilascio il semaforo
+      xSemaphoreGive(taskSyncPinSemaphore);
     }
 
-    delay(300000);  // Wait 5 minutes before reading again
+    vTaskDelay(300000);  // Attendi 5 minuti prima di leggere nuovamente
   }
 }
 
-void loop() {
-  //ADDED*****************************************************************
-  ArduinoOTA.handle(); // OTA programming handle
-  if (!client.connected()) { //mqtt connection handling
-    reconnect();
-  } else if (client.connected() && mqttCheckinTimer.isReady()) {                  // Check is ready a timer, send checkin to MQTT server
-    client.publish("momy-bot/checkIn", "OK");
-    mqttCheckinTimer.reset();                        // Reset a second timer
+void StreamingTask(void* param) {
+  //uxTaskGetStackHighWaterMark(NULL); // check how much of that space is/was used
+  WiFiManagerParameter serverParam("server", "Server IP", server_ip, 15);
+  WiFiManagerParameter portParam("port", "Server Port", String(server_port).c_str(), 6);
+
+  // Add parameters to WiFiManager
+  wm.addParameter(&serverParam);
+  wm.addParameter(&portParam);
+
+  // Try to connect to WiFi or start configuration portal
+  if (wm.autoConnect()) {
+    Serial.println("Connected to WiFi");
+
+    // Retrieve the configured server IP and port
+    strcpy(server_ip, serverParam.getValue());
+    server_port = atoi(portParam.getValue());
+
+    // Rest of your streaming task code...
+    if (client.connect(server_ip, server_port)) {
+      Serial.println("Connected to server");
+
+      camera_fb_t *fb = esp_camera_fb_get();
+      if (fb) {
+        // Rest of your streaming task code...
+      } else {
+        Serial.println("Error capturing image from camera");
+      }
+
+      client.stop();
+    } else {
+      Serial.println("Unable to connect to server");
+    }
+  } else {
+    Serial.println("Failed to connect to WiFi");
   }
-  client.loop();
-  //************************************************************************
+
+  vTaskDelay(1000 / frameRate);
+}
+
+void loop() {
   delay(50);
 }
 
@@ -399,65 +436,78 @@ void configModeCallback(WiFiManager* myWiFiManager) {
 
   tft.fillScreen(TFT_BLACK);
 }
-//ADDED*************************************************************************************
-void reconnect() { //MQTT reconnect handle
-  // Loop until we're reconnected
-  while (!client.connected()) {
-    Serial.print("Attempting MQTT connection...");
-    // Attempt to connect
-    if (client.connect("momy-bot")) {
-      Serial.println("connected");
-      // Subscribe
-      client.subscribe("momy-bot/eyes");
-      Serial.println("MQTT Checkin Timer set for 5 minutes");
-      // Set checkin timer
-      mqttCheckinTimer.setInterval(300000); // 5 minutes
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
-      delay(5000);
-    }
-  }
-}
 
-void mqttcallback(char* topic, byte* message, unsigned int length) {  //MQTT handle topic digestions
-  Serial.print("Message arrived on topic: ");
-  Serial.print(topic);
-  Serial.print(". Message: ");
-  String messageTemp;
+void cameraSetup() {
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.frame_size = FRAMESIZE_HVGA;
+  config.pixel_format = PIXFORMAT_JPEG; // for streaming
+  config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
+  config.fb_location = CAMERA_FB_IN_PSRAM;
+  config.jpeg_quality = 10;
+  config.fb_count = 1;
   
-  for (int i = 0; i < length; i++) {
-    Serial.print((char)message[i]);
-    messageTemp += (char)message[i];
+  // if PSRAM IC present, init with UXGA resolution and higher JPEG quality
+  // for larger pre-allocated frame buffer.
+  if(psramFound()){
+    config.jpeg_quality = 10;
+    config.fb_count = 2;
+    config.grab_mode = CAMERA_GRAB_LATEST;
+  } else {
+    // Limit the frame size when PSRAM is not available
+    config.frame_size = FRAMESIZE_CIF;
+    config.fb_location = CAMERA_FB_IN_DRAM;
   }
-  Serial.println();
 
-  // Feel free to add more if statements to control more GPIOs with MQTT
-
-  // If a message is received on the topic esp32/output, you check if the message is either "on" or "off". 
-  // Changes the output state according to the message
-  if (String(topic) == "momy-bot/eyes") {
-    Serial.print("Changing output to ");
-    if(messageTemp == "wink"){
-      Serial.println("wink");
-      wink_eyes();
-      delay(200);
-    }
-    else if(messageTemp == "sleep"){
-      Serial.println("sleep");
-      sleep_eyes();
-      delay(750);
-    }
-    else if(messageTemp == "close"){
-      Serial.println("close");
-      close_eyes();
-      delay(500);
-    }
-    else {
-      Serial.println(" -- UNKNOWN");
-    }
+  // camera init
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return;
   }
+
+  sensor_t * s = esp_camera_sensor_get();
+  // initial sensors are flipped vertically and colors are a bit saturated
+  s->set_brightness(s, 0);     // -2 to 2
+  s->set_contrast(s, 0);       // -2 to 2
+  s->set_saturation(s, 0);     // -2 to 2
+  s->set_special_effect(s, 0); // 0 to 6 (0 - No Effect, 1 - Negative, 2 - Grayscale, 3 - Red Tint, 4 - Green Tint, 5 - Blue Tint, 6 - Sepia)
+  s->set_whitebal(s, 1);       // 0 = disable , 1 = enable
+  s->set_awb_gain(s, 1);       // 0 = disable , 1 = enable
+  s->set_wb_mode(s, 0);        // 0 to 4 - if awb_gain enabled (0 - Auto, 1 - Sunny, 2 - Cloudy, 3 - Office, 4 - Home)
+  s->set_exposure_ctrl(s, 1);  // 0 = disable , 1 = enable
+  s->set_aec2(s, 0);           // 0 = disable , 1 = enable
+  s->set_ae_level(s, 0);       // -2 to 2
+  s->set_aec_value(s, 300);    // 0 to 1200
+  s->set_gain_ctrl(s, 1);      // 0 = disable , 1 = enable
+  s->set_agc_gain(s, 0);       // 0 to 30
+  s->set_gainceiling(s, (gainceiling_t)0);  // 0 to 6
+  s->set_bpc(s, 0);            // 0 = disable , 1 = enable
+  s->set_wpc(s, 1);            // 0 = disable , 1 = enable
+  s->set_raw_gma(s, 1);        // 0 = disable , 1 = enable
+  s->set_lenc(s, 1);           // 0 = disable , 1 = enable
+  s->set_hmirror(s, 0);        // 0 = disable , 1 = enable
+  s->set_vflip(s, 0);          // 0 = disable , 1 = enable
+  s->set_dcw(s, 1);            // 0 = disable , 1 = enable
+  s->set_colorbar(s, 0);       // 0 = disable , 1 = enable
+
+  Serial.println("Camera configuration complete!");
 }
-//*********************************************************************************************************************
